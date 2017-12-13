a976ab9f2d44d4eec5b533d4434175172c1fa861
anish2454 <shenoy.anish.227@gmail.com>
2017-12-12 22:26:23 -0500

    Added dist

--- find.js
+++ find.js
@@ -1,4 +1,5 @@
 var box = document.getElementById("box");
+var bg = document.getElementsByClassName("full")[0];
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
 
@@ -14,11 +15,21 @@ console.log( "box width: " + boxWidth );
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
   /* YOUR IMPLEMENTATION */
+  var xdiff = x0 - x1;
+  var ydiff = y0 - y1;
+  var dist = Math.sqrt((xdiff * xdiff) + (ydiff * ydiff));
+  return dist;
 };
 
 
 var findIt = function(e) {
   /* YOUR IMPLEMENTATION */
+  var x = e.x;
+  var y = e.y;
+  //console.log("X: " + x);
+  //console.log("Y: " + y);
+  var dist = distance(x, y, targetX, targetY);
+  console.log(dist);
 };
 
 /*
@@ -27,4 +38,3 @@ your OTHER FXNS
 */
 
 box.addEventListener("mousemove", findIt);
-
