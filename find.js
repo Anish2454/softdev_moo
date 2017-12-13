6b8bde5f43cb0a8964767aa5ec2435aad8f6a2b2
Anish2454 <shenoy.anish.227@gmail.com>
2017-12-13 13:18:13 -0500

    Added stuff

--- find.js
+++ find.js
@@ -5,8 +5,8 @@ var boxWidth = box.offsetWidth;
 
 //hardcode target as center
 //randomize later
-var targetX = boxWidth / 2;
-var targetY = boxHeight / 2;
+var targetX = Math.floor(Math.random() * boxWidth);
+var targetY = Math.floor(Math.random() * boxHeight);
 
 
 console.log( "box height: " + boxHeight );
@@ -21,15 +21,33 @@ var distance = function (x0, y0, x1, y1) {
   return dist;
 };
 
+var maxDist = function (targetX, targetY) {
+	
+	var corner1 = distance(targetX, targetY, 0, 0);
+	var corner2 = distance(targetX, targetY, 0, boxHeight);
+	var corner3 = distance(targetX, targetY, boxWidth, 0);
+	var corner4 = distance(targetX, targetY, boxWidth, boxHeight);
+	var maxCorner = Math.max(corner1, corner2, corner3, corner4);
+	return maxCorner;
+
+};
 
 var findIt = function(e) {
   /* YOUR IMPLEMENTATION */
   var x = e.x;
   var y = e.y;
+  if (distance(x, y, targetX, targetY) < 20 || true){
+  	
+  	console.log("FOUND!");
+  	box.innerHTML += "<div id='atom' style='position: absolute; left = " + targetX + " ; top = " + targetY + "; background-image: https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiWq6TEy4fYAhWnQ98KHZGwDUUQjRwIBw&url=https%3A%2F%2Fdiscuss.atom.io%2Ft%2Fnew-atom-logo-and-icon%2F8704&psig=AOvVaw214lx_eQdOzz1dV9souk4h&ust=1513275164375019;'></div>"; 
+  	bg.setAttribute("style", "background-color: rgb(" + dist +", " + dist + ", " + 255 + " );");
+  }
+  var maxD = maxDist(targetX, targetY);
   //console.log("X: " + x);
   //console.log("Y: " + y);
-  var dist = distance(x, y, targetX, targetY);
-  console.log(dist);
+  var dist = 255 - (Math.floor(distance(x, y, targetX, targetY) * (255 / maxD)));
+  //console.log(dist);
+  bg.setAttribute("style", "background-color: rgb(" + dist +", " + dist + ", " + 255 + " );");
 };
 
 /*
